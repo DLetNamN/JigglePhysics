3da2614ea42dd251257a80128b1133e01056710e
DLetNamN <wissam.hattab@elev.ga.lbs.se>
2019-09-18 09:37:44 +0200

    Imported my Spel 1.0.

---
+++ Spel 1.0/Assets/Scripts/GroundCheck.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour
+{
+    public bool grounded;
+    public int maxjumps = 2;
+    public Movement movement;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Ground")
+        {
+            movement.jumps = maxjumps;
+            grounded = true;
+        }
+    }
+}
---
+++ Spel 1.0/Assets/Scripts/Movement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [Range(0, 20f)]
+    public float movespeed;
+    public float jumpHeight;
+    public int jumps;
+
+    public GroundCheck groundCheck;
+
+    private Rigidbody2D rbody;
+
+    // Use this for initialization
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        rbody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * movespeed, rbody.velocity.y);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+
+    }
+
+    void Jump()
+    {
+        if (jumps > 0)
+        {
+            rbody.velocity = new Vector2(rbody.velocity.x, jumpHeight);
+            groundCheck.grounded = false;
+            jumps -= - 1;
+
+        }
+        if (jumps == 0)
+        {
+            return;
+        }
+    }
+    
+}
