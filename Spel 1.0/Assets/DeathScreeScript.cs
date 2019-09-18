d9dcdf22bb14bd38519e9b206a5e78b8b6a7633b
Sebastian Eking <sebastian.eking@elev.ga.lbs.se>
2019-09-18 09:50:59 +0200

    All the good shit

---
+++ Spel 1.0/Assets/DeathScreeScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathScreeScript : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Spel 1.0/Assets/PlayerHP.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerHP : MonoBehaviour
+{
+
+    public int playerHP = 3;
+
+    void Update()
+    {
+        if(playerHP <= 0)
+        {
+            SceneManager.LoadScene("DeathScreen");
+        }
+    }
+
+    public void OnCollisionEnter2D(Collision2D _collision)
+    {
+        if (_collision.gameObject.tag == "Enemy")
+        {
+            DamagePlayer();
+        }
+
+        if (_collision.gameObject.tag == "Heal")
+        {
+            HealPlayer();
+        }
+    }
+
+
+    public void DamagePlayer()
+    {
+        playerHP -= 1;
+
+        Debug.Log("Damage acquired");
+    }
+
+    public void HealPlayer()
+    {
+        playerHP += 1;
+
+        if(playerHP >= 3)
+        {
+            playerHP = 3;
+        }
+
+        Debug.Log("HP acquired");
+    }
+
+}
