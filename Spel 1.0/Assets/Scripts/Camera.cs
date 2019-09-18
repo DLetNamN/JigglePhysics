0af044e3bed38bd912d20e4d6aa8b0a133ed2eff
DLetNamN <wissam.hattab@elev.ga.lbs.se>
2019-09-18 10:52:42 +0200

    Started screenshake and camera work. Doonka boonka

--- Spel 1.0/Assets/Scripts/Camera.cs
+++ Spel 1.0/Assets/Scripts/Camera.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class Camera : MonoBehaviour
 {
+    public Transform playerTransform;
+    public Vector3 cameraOffset = new Vector3(3, 3.6f, -10);
+    public Transform cameraTransform;
+    public GameObject playerObject;
+
     void Start()
     {
-        
+        playerObject = GameObject.FindWithTag("Player");
+        playerTransform = playerObject.GetComponent<Transform>();
+        cameraTransform = GetComponentInParent<Transform>();
     }
 
     void Update()
     {
-        
+        playerTransform = playerObject.GetComponent<Transform>();
+        cameraTransform.position = new Vector3(playerTransform.position.x, playerTransform.position.y + cameraOffset.y, playerTransform.position.y + cameraOffset.z);
     }
 }
---
+++ Spel 1.0/Assets/Scripts/CameraFeedback.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFeedback : MonoBehaviour
+{
+    public 
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
