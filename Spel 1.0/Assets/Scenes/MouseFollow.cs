1d398ae26f84a8b88f574cb8b1700aa1fee220c2
Sven Viktor Krantz <viktor.krantz@elev.ga.lbs.se>
2019-09-25 08:48:00 +0200

    Fixade så att man kan nu sjkuta och man kan nu lägga till ljud väldigt enkelt

---
+++ Spel 1.0/Assets/Scenes/MouseFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseFollow : MonoBehaviour
+{
+    public Vector3 mousePosition;
+    private Rigidbody2D rb;
+    public Vector2 direction;
+    public float moveSpeed;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        direction = (mousePosition - transform.position).normalized;
+        rb.velocity = new Vector2(direction.x * moveSpeed, direction.y * moveSpeed); ;
+    }
+}
---
+++ Spel 1.0/Assets/Scripts/LookAtMouse.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtMouse : MonoBehaviour
+{
+    public float offset;
+    public GameObject Bullet;
+    public Transform ShotPoint;
+    public float TimeBtwShots;
+    public float StartTimeBtwShots;
+    private AudioManager audioManager;
+
+    private void Update()
+    {
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotZ + offset);
+
+        if (TimeBtwShots <= 0)
+        {
+            if (Input.GetMouseButton(0) && TimeBtwShots <= 0)
+            {
+                Instantiate(Bullet, ShotPoint.position, transform.rotation);
+                TimeBtwShots = StartTimeBtwShots;
+                FindObjectOfType<AudioManager>().Play("ShootingSound");
+                
+            }
+        }
+        
+        else if (TimeBtwShots > 0)
+        {
+            TimeBtwShots = TimeBtwShots - Time.deltaTime;
+        }
+    }
+}
