c6dd2e10ecea27488382e39bd11ba091c6ede5d4
DLetNamN <wissam.hattab@elev.ga.lbs.se>
2019-09-26 11:55:50 +0200

    Created proper camera movement

--- Spel 1.0/Assets/Scripts/cameraMovement.cs
+++ Spel 1.0/Assets/Scripts/cameraMovement.cs
@@ -5,32 +5,37 @@ using UnityEngine;
 public class cameraMovement : MonoBehaviour
 {
     public Transform playerTransform;
-    public Vector3 cameraOffset = new Vector3(3, 3.6f, -10);
     public Transform cameraTransform;
     public GameObject playerObject;
 
     public Rigidbody2D playerRigidbody;
     public Vector3 playerCameraOffset;
+    public float cameraSpeed = 1.0f;
 
-    public void cameraFollow()
-    {
-        float velocityX = playerRigidbody.velocity.x;
-        float velocityY = playerRigidbody.velocity.y;
-        float positionX = playerTransform.position.x;
-        float positionY = playerTransform.position.y;
-
-        playerTransform = playerObject.GetComponent<Transform>();
-        cameraTransform.position = new Vector3(positionX - velocityX, positionY + cameraOffset.y - velocityY, cameraOffset.z);
-    }
-
-    void Start()
+    public void cameraVariables()
     {
         playerObject = GameObject.FindWithTag("Player");
         playerTransform = playerObject.GetComponent<Transform>();
         cameraTransform = GetComponentInParent<Transform>();
+        playerRigidbody = playerObject.GetComponent<Rigidbody2D>();
+        playerCameraOffset = new Vector3(0, 0, -10);
     }
 
-    void Update()
+    public void Start()
+    {
+        cameraVariables();
+    }
+
+    public void cameraFollow()
+    {
+        Vector3 playerTransf = playerTransform.position;
+
+        Vector3 DesiredPosition = playerTransf + playerCameraOffset;
+
+        cameraTransform.position = Vector3.Lerp(playerTransf + playerCameraOffset, DesiredPosition, cameraSpeed);
+    }
+
+    public void FixedUpdate()
     {
         cameraFollow();
     }
