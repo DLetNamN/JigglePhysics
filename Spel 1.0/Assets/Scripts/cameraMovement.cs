c04ed02b9b61d990be88a1bf18c21823b54b5acd
DLetNamN <wissam.hattab@elev.ga.lbs.se>
2019-10-02 08:42:29 +0200

    No screenshake yet

--- Spel 1.0/Assets/Scripts/cameraMovement.cs
+++ Spel 1.0/Assets/Scripts/cameraMovement.cs
@@ -4,14 +4,23 @@ using UnityEngine;
 
 public class cameraMovement : MonoBehaviour
 {
+    //Game variables.
     public Transform playerTransform;
     public Transform cameraTransform;
     public GameObject playerObject;
-
     public Rigidbody2D playerRigidbody;
+
+    //Camera variables.
     public Vector3 playerCameraOffset;
     public float cameraSpeed = 1.0f;
 
+    //Screenshake variables.
+    private float shakeTime = 0f;
+    private float shakeSize = 0.7f;
+    private float speed = 0.5f;
+    public Vector3 initialPosition;
+
+    //To keep variable edits out of the way.
     public void cameraVariables()
     {
         playerObject = GameObject.FindWithTag("Player");
@@ -29,14 +38,19 @@ public class cameraMovement : MonoBehaviour
     public void cameraFollow()
     {
         Vector3 playerTransf = playerTransform.position;
-
+        
         Vector3 DesiredPosition = playerTransf + playerCameraOffset;
 
         cameraTransform.position = Vector3.Lerp(playerTransf + playerCameraOffset, DesiredPosition, cameraSpeed);
+
     }
 
     public void FixedUpdate()
     {
         cameraFollow();
+    }
+
+    public void Update()
+    {
     }
 }
