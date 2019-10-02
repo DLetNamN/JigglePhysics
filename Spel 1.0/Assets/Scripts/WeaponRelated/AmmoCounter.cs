a3e5006cb7cb1c3613ee80fa5aca96fe2f6bdd7b
Sven Viktor Krantz <viktor.krantz@elev.ga.lbs.se>
2019-10-02 08:04:43 +0200

    fixade alla grejer

---
+++ Spel 1.0/Assets/Scripts/WeaponRelated/AmmoCounter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AmmoCounter : MonoBehaviour
+{
+    public Text ammoAmount;
+    public LookAtMouse lam;
+    void Update()
+    {
+        string MaxAmmo = (lam.MaxShots).ToString();
+        string currentAmmo = (lam.MaxShots - lam.shotAmount).ToString();
+        ammoAmount.text = MaxAmmo + "/" + currentAmmo;
+
+        if(lam.reloadTimer > 0)
+        {
+            ammoAmount.text = "N/A";
+        }
+    }
+}
