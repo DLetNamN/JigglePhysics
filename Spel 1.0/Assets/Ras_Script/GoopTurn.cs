ca20d52c9f181f1aac85d8bdb7234e1002768ce3
Rasmus <rasmus.grinsvall@elev.ga.lbs.se>
2019-10-02 09:19:24 +0200

    Added sprites

---
+++ Spel 1.0/Assets/Ras_Script/GoopTurn.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoopTurn : MonoBehaviour
+{
+    public SlimeGoop slimeGoop;
+
+    public Transform slimeTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        slimeTransform = GetComponentInParent<Transform>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(slimeGoop.WhereToLook == true)
+        {
+            slimeTransform.localEulerAngles = new Vector3(0f, 0f, -180f);
+        }
+        else
+        {
+            slimeTransform.localEulerAngles = new Vector3(0f, 180f, -180f);
+        }
+    }
+}
--- Spel 1.0/Assets/Ras_Script/SlimeGoop.cs
+++ Spel 1.0/Assets/Ras_Script/SlimeGoop.cs
@@ -14,6 +14,8 @@ public class SlimeGoop : MonoBehaviour
     public int HowManyJump;
     public Vector2 slimeJumpLeft;
     public Vector2 slimeJumpRight;
+
+    public GoopAqush goopSquish;
     // Start is called before the first frame update
     void Start()
     {
@@ -38,25 +40,26 @@ public class SlimeGoop : MonoBehaviour
         {
             rBody.AddForce(slimeJumpLeft);
             HowManyJump = HowManyJump + 1;
-             
+            goopSquish.SquishTimer = 0;
         }
         else if (WhereToLook == false && HowManyJump >= 3 && goopCheck.goopOnGround == true || Input.GetKey(KeyCode.H))
         {
             rBody.AddForce(slimeJumpLeft * 2);
             HowManyJump = 0;
-            
+            goopSquish.SquishTimer = 0;
         }
 
         if (WhereToLook == true && HowManyJump < 3 && goopCheck.goopOnGround == true || Input.GetKey(KeyCode.H))
         {
             rBody.AddForce(slimeJumpRight);
             HowManyJump = HowManyJump + 1;
-            
+            goopSquish.SquishTimer = 0;
         }
         else if (WhereToLook == true && HowManyJump >= 3 && goopCheck.goopOnGround == true || Input.GetKey(KeyCode.H))
         {
             rBody.AddForce(slimeJumpRight * 2);
             HowManyJump = 0;
+            goopSquish.SquishTimer = 0;
         }
     }
 }
